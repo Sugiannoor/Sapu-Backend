7ed6d2b515b9cc359a1bdccaa01703da985fa6ba
Akhmad Sugiannoor <AkhmadSugiannoor9@gmail.com>
2023-11-17 11:38:58 +0800

    Menggunakan API Dummy Untuk RestFul API sementara

--- src/pages/login/components/LoginForm.jsx
+++ src/pages/login/components/LoginForm.jsx
@@ -4,16 +4,37 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 import "./loginForm.css";
 import { ButtonElement } from "../../../components/elements/button";
 import InputElement from "../../../components/elements/input/InputElement";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [msg, setMsg] = useState ('');
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState (false);
+  // URL Endpoint API
+  const url = "https://dummyjson.com/auth/login"; 
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    //Logic Ketika data Terkirim email dan Password
+    setIsLoading (true)
+
+    try {
+      const response = await axios.post(url, {
+        email: email,
+        password: password,
+      });
+      setIsLoading(false)
+      console.log(response.data);
+      navigate("/beranda");
+    } catch (error) {
+      setIsLoading (false);
+      if (error.response) {
+        setMsg(error.response.data.message);
+      }
+    }
   };
 
   const handleChange = (e) => {
@@ -72,17 +93,20 @@ const LoginForm = () => {
             )}
           </span>
         </div>
+        <div className="feedback-login mt-2 text-danger">{msg}</div>
         <ButtonElement
           handleClick={handleSubmit}
-          isLoading={false}
+          isLoading={isLoading}
           type="submit"
           className="btn btn-success mt-5 w-100"
         >
           Login
         </ButtonElement>
       </form>
-      <Link to= "/registrasi">
-      <span className="d-flex justify-content-center mt-3">Belum Punya akun?</span>
+      <Link to="/registrasi">
+        <span className="d-flex justify-content-center mt-3">
+          Belum Punya akun?
+        </span>
       </Link>
     </>
   );
